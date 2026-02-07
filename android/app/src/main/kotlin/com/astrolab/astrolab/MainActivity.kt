49ccc36edf1ab0bad9c3de0f2bbcf8dce5c7574c
imm1h01 <mihai.condrici@gmail.com>
2026-02-07 16:59:53 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/astrolab/astrolab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.astrolab.astrolab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/astrolab/astrolab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.astrolab.astrolab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
